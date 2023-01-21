62e53d488267529ca10c81ad5a15efc684fe77e4
Fathimath-Rasmina <rasminaf58@gmail.com>
2023-01-21 16:20:07 +0530

    cores added

--- backend/settings.py
+++ backend/settings.py
@@ -179,8 +179,12 @@ MEDIA_ROOT = 'static/images'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "https://myshopn.netlify.app",
+   
+]
 
 # RAZORPAY_PUBLIC_KEY = 'rzp_test_CBjhFV8rtJcEfo'
 # RAZORPAY_SECRET_KEY = 'SVam4dGT09upQ0yssaHGMgAI'
